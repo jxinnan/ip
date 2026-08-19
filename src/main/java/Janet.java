5a8d8fcc1de0c6b91679f708820e040855a6c89c
jxinnan <jiangxinnan416@gmail.com>
2026-08-20 00:05:22 +0800

    Rename Duke.java to Janet.java

---
+++ src/main/java/Janet.java
@@ -0,0 +1,11 @@
+public class Janet {
+    public static void main(String[] args) {
+        String banner = "     _                  _\n"
+                + "    | |                | |\n"
+                + "    | | __ _ _ __   ___| |_\n"
+                + " _  | |/ _` | '_ \\ / _ \\ __|\n"
+                + "| |_| | (_| | | | |  __/ |_\n"
+                + " \\___/ \\__,_|_| |_|\\___|\\__|\n";
+        System.out.println(banner);
+    }
+}
