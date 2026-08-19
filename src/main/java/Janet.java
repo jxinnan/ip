ac1ad54b3a2cd40bacc2c7bcfc2c32b5925794f3
jxinnan <jiangxinnan416@gmail.com>
2026-08-20 00:38:12 +0800

    Janet.java: Add ability to store and display tasks

--- src/main/java/Janet.java
+++ src/main/java/Janet.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
  * A simple command-line chatbot that echoes user input until asked to leave.
  */
 public class Janet {
+    /** Maximum number of tasks Janet can store during one run. */
+    private static final int MAX_TASKS = 3;
+
     public static void main(String[] args) {
         String banner = "____________________________________________________________\n"
                 + "     _                  _\n"
@@ -18,6 +21,8 @@ public class Janet {
                 + "____________________________________________________________\n";
         System.out.print(banner);
 
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -30,7 +35,18 @@ public class Janet {
                 break;
             }
 
-            System.out.println(" " + command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
+            } else {
+                System.out.println(" Sorry, I can only store " + MAX_TASKS + " tasks.");
+            }
+
             System.out.println("____________________________________________________________");
         }
     }
