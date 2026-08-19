3d8ebc70809e9705ec63bd26b19990f67af1c4bc
jxinnan <jiangxinnan416@gmail.com>
2026-08-20 00:11:42 +0800

    Janet.java: Add greeting and farewell

--- src/main/java/Janet.java
+++ src/main/java/Janet.java
@@ -1,11 +1,18 @@
 public class Janet {
     public static void main(String[] args) {
-        String banner = "     _                  _\n"
+        String banner = "____________________________________________________________\n"
+                + "     _                  _\n"
                 + "    | |                | |\n"
                 + "    | | __ _ _ __   ___| |_\n"
                 + " _  | |/ _` | '_ \\ / _ \\ __|\n"
                 + "| |_| | (_| | | | |  __/ |_\n"
-                + " \\___/ \\__,_|_| |_|\\___|\\__|\n";
-        System.out.println(banner);
+                + " \\___/ \\__,_|_| |_|\\___|\\__|\n"
+                + "____________________________________________________________\n"
+                + "Hi! I'm Janet! I'm here to help with absolutely anything.\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Okay! Have a wonderful day. Bye!\n"
+                + "____________________________________________________________\n";
+        System.out.print(banner);
     }
 }
